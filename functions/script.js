b98336a91999abc8724305114258dfb60c4faf62
HugoEmanuelLC <helc85.info@gmail.com>
2023-09-21 17:58:16 +0200

    fncts

---
+++ functions/script.js
@@ -0,0 +1,30 @@
+// let varTest = "var test"
+// function lamyFunctionName(txt){
+//     return console.log("text test " , txt, varTest);
+// }
+
+// lamyFunctionName("do")
+// lamyFunctionName("re")
+// lamyFunctionName("mi")
+// console.log(lamyFunctionName);
+// console.log(parseInt);
+
+// let test = ['eeeee',"tttttttttt"]
+// console.log(test.fill);
+
+
+
+
+// let myFnct = function(){
+//     return "var fnct"
+// }
+// console.log(myFnct);
+// console.log(myFnct());
+
+
+
+
+let testFnct = function name() {
+    return "test fnct / name"
+}
+console.log(testFnct());
