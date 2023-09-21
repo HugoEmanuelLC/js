e1e48464419f8d3cfe6a39016f7809707d014e2f
HugoEmanuelLC <helc85.info@gmail.com>
2023-09-21 10:20:36 +0200

    fni random nbr

--- loops/magic-number/script.js
+++ loops/magic-number/script.js
@@ -1,40 +1,34 @@
 
 
 let numberMaximun = prompt("when many numbers ?"); //string
-numberMaximun = parseInt(numberMaximun); //number
 console.log(typeof numberMaximun);
 console.log(numberMaximun);
 
 let magicNumber = Number;
 
-while (Number.isNaN(numberMaximun) == true) {
+while (Boolean(Number(numberMaximun)) == false) {
     alert("you must only use numbers")
     numberMaximun = prompt("you play with so many numbers ?")
-    numberMaximun = parseInt(numberMaximun);
 }
 magicNumber = Math.floor(Math.random() * numberMaximun) + 1;
 
 let myNumber = prompt('find the number :-P')
-myNumber = parseInt(myNumber)
 
 let winer = false;
 
 do {
-    while (Number.isNaN(myNumber) == true) {
+    while (Boolean(Number(myNumber)) == false) {
         alert("you must only use numbers")
         myNumber = prompt('find the number :-P')
-        myNumber = parseInt(myNumber)
     }
     if (myNumber > magicNumber ) {
         console.log("plus petit")
         alert("too big :-D")
         myNumber = prompt('find the number :-P')
-        myNumber = parseInt(myNumber)
     }else if(myNumber < magicNumber ) {
         console.log("plus grand")
         alert("too small :-\ ")
         myNumber = prompt('find the number :-P')
-        myNumber = parseInt(myNumber)
     }else{
         winer = true
         break
---
+++ loops/toDoList/script.js
@@ -0,0 +1,34 @@
+
+//Demander au user de choisir ça tache
+
+//new: to add a todo
+// list: to see all the todos
+// delete: to remove a specific todo
+// quit: to exit de program
+
+
+//infos::::::::
+//delete:
+//supression par l'id de la liste
+//quit:
+//sortir de la boucle
+
+
+
+//Switch pour le choix du user
+
+
+
+//La liste du user, exemple:
+// ********* TODO LIST *********
+// 1. Walk out the dog
+// 2. Drink some milk
+// 3. Learn JavaScript
+// 4. Make exercises
+// etc...
+
+
+
+
+let stain = "50";
+console.log(Boolean(Number(stain)));
