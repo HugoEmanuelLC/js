06961ce163bcdb759d0d119b763f41123a72fa1b
HugoEmanuelLC <helc85.info@gmail.com>
2023-09-21 12:45:46 +0200

    exo todolist

--- arrays/script.js
+++ arrays/script.js
@@ -1,3 +1,3 @@
 let supers = ['superman', 'batman', 'flash', 'aquaman'];
-supers.splice()
+supers.splice(1,1)
 console.log(supers)
--- loops/toDoList/script.js
+++ loops/toDoList/script.js
@@ -1,7 +1,7 @@
 
 //Demander au user de choisir ça tache
 
-//new: to add a todo
+// new: to add a todo
 // list: to see all the todos
 // delete: to remove a specific todo
 // quit: to exit de program
@@ -29,6 +29,103 @@
 
 
 
+// alert("welcome to the game ! 8-D ")
+let quit = false;
+let userChoice;
+let tabToDoList= []
+let nbrItem = 1
+let tampon = 0;
 
-let stain = "50";
-console.log(Boolean(Number(stain)));
+userChoice = prompt(`
+    Chose your tasks:
+        new: to add a todo
+        list: to see all the todos
+        delete: to remove a specific todo
+        quit: to exit de program
+`);
+
+do {
+    switch (userChoice) {
+        case 'new':
+            tabToDoList.push(prompt(`you can add your item`))
+            userChoice = prompt(`
+                Chose your tasks:
+                    new: to add a todo
+                    list: to see all the todos
+                    delete: to remove a specific todo
+                    quit: to exit de program
+            `);
+            break;
+
+        case 'list':
+            if (tabToDoList.length == 0) {
+                alert('No items in the list')
+            }else{
+                let element = "";
+                for (let i = 0; i < tabToDoList.length; i++) {
+                    tampon = nbrItem + i;
+                    element += ` \n ${tampon}. ${tabToDoList[i]} `;
+                }
+                alert("********* TODO LIST *********"+element)
+            }
+            userChoice = prompt(`
+                Chose your tasks:
+                    new: to add a todo
+                    list: to see all the todos
+                    delete: to remove a specific todo
+                    quit: to exit de program
+            `);
+            break;
+
+        case 'delete':
+            let idNbr = Number;
+            let element = "";
+            for (let i = 0; i < tabToDoList.length; i++) {
+                tampon = nbrItem + i;
+                element += ` \n ${tampon}. ${tabToDoList[i]} `;
+            }
+            idNbr = prompt(`
+                Indicate the element number to delete \n
+                ********* TODO LIST *********
+                ${element}
+            `)
+            
+            if (Boolean(tabToDoList.find(ele => {
+                return ele[idNbr]
+            }))){
+                tabToDoList.splice(idNbr, 1)
+            }else{
+                alert("element not found")
+            }
+            userChoice = prompt(`
+                Chose your tasks:
+                    new: to add a todo
+                    list: to see all the todos
+                    delete: to remove a specific todo
+                    quit: to exit de program
+            `);
+            break;
+
+        case 'quit':
+            quit = true;
+            break;
+
+        default:
+            alert('Sorry, but, incorrect indication !!')
+            userChoice = prompt(`
+                Chose your tasks:
+                    new: to add a todo
+                    list: to see all the todos
+                    delete: to remove a specific todo
+                    quit: to exit de program
+            `);
+            break;
+    }
+    console.log(tabToDoList)
+} while (quit == false);
+
+alert("Thank you and see you soon !!!")
+
+
+
+
