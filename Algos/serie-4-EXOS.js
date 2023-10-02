252926ce5997bfe08225c15c3a5054bafa9312b3
HugoEmanuelLC <helc85.info@gmail.com>
2023-10-02 20:50:42 +0200

    edit exos

--- Algos/serie-4-EXOS.js
+++ Algos/serie-4-EXOS.js
@@ -20,25 +20,102 @@
 
 
 //serie-4 Advanced EXO 01
-function ransomNote(noteText, magazineText) {
-    
-}
-noteText = "this is a secret note to you from a secret admirer"
-magazineText = "puerto rico is a great place you must hike far from town to find a secret waterfall that i am an admirer of but note that it is not as hard as it seems this is my advice to you"
-ransomNote(noteText, magazineText)
+// function ransomNote(noteText='', magazineText="") {
+//     let tabMagazine = magazineText.split(' ')
+//     let tabNote = noteText.split(' ')
+//     let rep = true;
+//     for (let i = 0; i < tabNote.length; i++) {
+//         if (tabMagazine.indexOf(tabNote[i]) != -1) {
+//             tabMagazine.splice(tabMagazine.indexOf(tabNote[i]),1)
+//         } else {
+//             rep = false
+//         }
+//     }
+//     return rep
+// }
+// noteText = "this is a secret note to you from a secret admirer"
+// magazineText = "puerto rico is a great place you must hike far from town to find a secret waterfall that i am an admirer of but note that it is not as hard as it seems this is my advice to you"
+// console.log(ransomNote(noteText, magazineText));
 
 
 
 
 //serie-4 Advanced EXO 02
-
-
+// function isPalindrome(params='') {
+//     let word = params.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, ``).replaceAll(' ','').toLowerCase()
+//     let tabWord = word
+//     tabWord = tabWord.split('').reverse()
+//     let reverseWord = ""
+//     tabWord.forEach(element => reverseWord += element);
+//     return word == reverseWord ? true : false
+// }
+// console.log(isPalindrome("kayak"));
+// console.log(isPalindrome("race car"));
+// console.log(isPalindrome("hello world"));
+// console.log(isPalindrome("Madam, I'm Adam"));
 
 
 
 
 
 //serie-4 Advanced EXO 03
+// function caesarCipher(str="", num=0) {
+//     const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+//     let tabStr = str.toUpperCase().split('')
+//     let tabNewWord = []
+//     let newWord ='';
+//     let test;
+//     for (let e of tabStr) {
+//         // for (let i = 0; i < alphabet.length; i++) {
+//         //     if (tabStr[e] == alphabet[i]) {
+//         //         if(i+num <= alphabet.length-1){
+//         //             if (i+num < 0) {
+//         //                 let id = i+num
+//         //                 id = alphabet.length+id
+//         //                 tabNewWord.push(alphabet[id])
+//         //             }else{
+//         //                 tabNewWord.push(alphabet[i+num])
+//         //             }
+//         //         }else{
+//         //             let tampon = i+num
+//         //             tampon = tampon-(alphabet.length-1)
+//         //             tabNewWord.push(alphabet[tampon-1])
+//         //         }
+//         //     }
+//         // }
+//         test = alphabet.indexOf(e)
+//         if (e == ' ') {
+//             tabNewWord.push(e)
+//         }
+//         else if (e == alphabet[test]) {
+//             if(test+num <= alphabet.length-1){
+//                 if (test+num < 0) {
+//                     let id = test+num
+//                     id = alphabet.length+id
+//                     tabNewWord.push(alphabet[id])
+//                 }else{
+//                     tabNewWord.push(alphabet[test+num])
+//                 }
+//             }else{
+//                 let tampon = test+num
+//                 tampon = tampon-(alphabet.length-1)
+//                 tabNewWord.push(alphabet[tampon-1])
+//             }
+//         }
+//     }
+//     newWord += tabNewWord
+//     newWord = newWord.replaceAll(',', '')
+//     return newWord
+// }
+// console.log(caesarCipher("Zoo keeper", 2));
+// console.log(caesarCipher("bqq mggrgt", -2));
+// console.log(caesarCipher("My name is Henrique", 3));
+// let tab = "hugo"
+// tab = tab.split('')
+// tab[0] = "j"
+// console.log(tab[0]);
+// let a = -2, b =0
+// console.log(b+a);
 
 
 
@@ -47,14 +124,36 @@ ransomNote(noteText, magazineText)
 
 
 //serie-4 Advanced EXO 04
-
-
+// function reverseWord(params="") {
+//     let tabWord = params.split(' ')
+//     let reverseWord="";
+//     let newWord = ""
+//     tabWord.forEach(elem => {
+//         reverseWord = elem.split('')
+//         newWord += reverseWord.reverse()+" "
+//     })
+//     newWord= newWord.toLowerCase().replaceAll(',','')
+//     return newWord
+// }
+// console.log(reverseWord("This is a string of words"));
 
 
 
 
 
 //serie-4 Advanced EXO 05
+// function reverseArray(params) {
+//     // console.log(params.length/2);
+//     for (let a = 0; a < params.length/2; a++) {
+//         // console.log(a);
+//         let tampo = params[a]
+//         params[a] = params[params.length-1-a]
+//         params[params.length-1-a] = tampo
+//     }
+//     return params
+// }
+// console.log(reverseArray(['hugo','lopes','clavinas']));
+// console.log(reverseArray(['hugo','lopes','clavinas', 'paire', 'plusgrand']));
 
 
 
@@ -62,3 +161,41 @@ ransomNote(noteText, magazineText)
 
 
 //serie-4 Advanced EXO 06
+// function twoSums(params){
+//     let tabSum = []
+//     console.log(params);
+//     for (let a = 0; a < params.length-1; a++) {
+//         for (let b = a; b < params.length; b++) {
+//             if (params[a]+params[b] == 7) {
+//                 tabSum.push([params[b],params[a]])
+//             } else {
+                
+//             }
+//         }
+//     }
+//     return tabSum
+// }
+// console.log(twoSums([1, 6, 4, 5, 3, 3]));
+
+
+
+
+
+
+//serie-4 Advanced EXO 07
+// function fibonacci(params=0) {
+//     let tab=[];
+//     let i = 0
+//     while (i<params) {
+//         if (tab.length >= 2) {
+//             tab.push(tab[i-2]+tab[i-1])
+//         } else {
+//             tab.push(1)
+//         }
+//         i++
+//     }
+//     return tab
+// }
+// console.log(fibonacci(4));
+// console.log(fibonacci(6));
+// console.log(fibonacci(9));
---
+++ Algos/serie-5-EXOS.js
@@ -0,0 +1,146 @@
+//serie-5 facultatif EXO 01
+// let removeDuplicates = (params=[]) => params.filter((ele, item) => params.indexOf(ele) === item)
+
+// console.log(removeDuplicates([4, 9, 5, 1, 3, 2, 4, 1, 8]));
+// // Result : [4, 9, 5, 1, 3, 2, 8]
+// console.log(removeDuplicates(["hello", "world", "goodbye", "world"]));
+// // Result : ['hello', 'world', 'goodbye']
+// console.log(removeDuplicates([true, true, false, true, true, false]));
+// // Result : [true, false]
+
+
+
+
+//serie-5 facultatif EXO 02
+// let capitalize = (params="") => params.toUpperCase()
+
+// console.log(capitalize("belgium"));
+// // Result : "Belgium"
+// console.log(capitalize("brazil"));
+// // Result : "Brazil"
+// console.log(capitalize("brussels"));
+// // Result : "Brussels"
+
+
+
+
+
+//serie-5 facultatif EXO 03
+// let dayDif =(d1, d2) => new Number((d2.getTime() / 86400000) - (d1.getTime() / 86400000)).toFixed(0);
+
+// console.log(dayDif(new Date("2020-10-21"), new Date("2021-10-22")));
+// Result : 366
+
+
+
+
+
+
+
+//serie-5 facultatif EXO 04
+// average =(...params) => (params.reduce((acc,elem) => acc + elem ))/params.length
+// console.log(average(1, 2, 3, 4));
+// Result: 2.5
+
+
+
+
+
+//serie-5 facultatif EXO 05
+// let getSmallest = (params) => Math.min(...params);
+// const arr = [13, 7, 11, 3, 9, 15, 17];
+// console.log(getSmallest(arr));
+// Result: 3
+
+
+
+
+
+//serie-5 facultatif EXO 06
+// let areEqual = (tb1, tb2) => tb1.reduce((acc, elem) => acc + elem) === tb2.reduce((acc, elem) => acc + elem);
+
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = [3, 1, 4, 2];
+// const arr3 = [1, 2, 3];
+
+// console.log(areEqual(arr1, arr2));
+// // Result : true
+// console.log(areEqual(arr1, arr3));
+// // Result : false
+
+
+
+
+
+
+//serie-5 Advanced EXO 07
+// let randomRGB = () => `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`
+// console.log(randomRGB());
+// // Result : "rgb(232, 115, 164)"
+
+
+
+
+
+//serie-5 Advanced EXO 08
+// let occurencies = (str="", nbr) => str.split('').filter(ele => ele==nbr).length
+// console.log(occurencies("hello", "l"));
+// // Result : 2
+// console.log(occurencies("abracadabra", "a"));
+// // Result : 5
+// console.log(occurencies("oups", "z"));
+// // Result : 0
+
+
+
+
+
+
+//serie-5 Advanced EXO 09
+// let onlyPositives = (params=[]) => params.reduce((acc, ele) => (acc<0 ? acc=0 : acc +(ele >= 0 ? ele : 0)) )
+// console.log(onlyPositives([1, 6, 2, -3, 5, -12]));
+// // Result : 14
+// console.log(onlyPositives([-3, -4, -2]));
+// // Result : 0
+
+
+
+
+
+
+//serie-5 Advanced EXO 10  element.Object.keys(element).forEach(ele => console.log(ele))
+let scanAndFind = (tbObj, obj) => tbObj.forEach(element =>  Object.keys(element).forEach(ele => ele == Object.keys(obj) ? console.log(Object.values(obj)) : null))
+
+console.log(scanAndFind(
+    [
+      {
+        firstName: "Vito",
+        lastName: "Corleone",
+      },
+      {
+        firstName: "Jon",
+        lastName: "Snow",
+      },
+      {
+        firstName: "Harry",
+        lastName: "Potter",
+      },
+      {
+        firstName: "Michal",
+        lastName: "Corleone",
+      },
+    ],
+    {lastName: "Corleone",}
+));
+
+console.log(scanAndFind(
+    [
+      { fullName: "Roi Baudoin", id: 49762 },
+      { fullName: "Margareth Tatcher", id: 94357 },
+      { fullName: "Barack Obama", id: 76458 },
+      { fullName: "Emmanuel Macron", id: 10687 },
+      { fullName: "Charles de Gaulle", id: 67098 },
+      { fullName: "Boris Johnson", id: 16437 },
+    ],
+    { id: 10687 }
+));
